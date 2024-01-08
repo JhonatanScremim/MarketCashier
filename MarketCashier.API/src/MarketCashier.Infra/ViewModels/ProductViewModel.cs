77a96ce499eaf5b775f184a48fc302f04fb0dc7a
Jhonatan <jhonatanscremim@gmail.com>
2024-01-08 16:55:22 -0300

    fix-api(product-paginated)
    Adiciona atributo Id na View Model para response do Product

--- MarketCashier.API/src/MarketCashier.Infra/ViewModels/ProductViewModel.cs
+++ MarketCashier.API/src/MarketCashier.Infra/ViewModels/ProductViewModel.cs
@@ -2,6 +2,7 @@ namespace MarketCashier.Infra.ViewModels
 {
     public class ProductViewModel
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Brand { get; set; }
         public double Value { get; set; }
