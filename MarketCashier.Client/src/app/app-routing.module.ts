070bdbc9cea35842b078c109e7efeba859d3ffac
Jhonatan <jhonatanscremim@gmail.com>
2024-01-10 21:44:28 -0300

    feat-client(routes-home)
    Inicia o desenvolvimento da rota home com lazy load

--- MarketCashier.Client/src/app/app-routing.module.ts
+++ MarketCashier.Client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./core/components/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./components/components.module').then((m) => m.ComponentsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MarketCashier.Client/src/app/app.component.ts
+++ MarketCashier.Client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
+  template: `<a [routerLink]="['login']">login</a>
   <router-outlet></router-outlet>
   `,
 })
